fad0970ab53a29bd9338209aee4ac04ec723b410
William Barrilli <felip.ew@hotmail.com>
2023-06-18 16:50:29 -0300

    init version

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,45 @@
+import ReactPlayer from "react-player";
+import "./styles.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="box-title">
+          <label className="title">Em um relacionamento</label>
+          <label className="title">saúdavel com o</label>
+          <label className="title-double">TDAH</label>
+          <label className="title"> & </label>
+          <label className="title"> A organização</label>
+        </div>
+        <div className="video-player">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=wX9AwH8WA3g"
+            controls={true}
+            playing
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
+
+        <label className="text">
+          Aprovada em menos de 30 dias a como modificar sua vida com essas 8
+          dicas práticas para quem acha que tem TDAH ou dificuldade em
+          organização e procrastinação
+        </label>
+
+        <div className="box-btn">
+          <button
+            className="btn"
+            onClick={() =>
+              window.open("https://chk.eduzz.com/1955919", "_blank")
+            }
+          >
+            Clique aqui é adquira essa oportunidade
+          </button>
+        </div>
+      </div>
+      {/* <source src="https://youtu.be/wX9AwH8WA3g" type="video/ogg" /> */}
+    </div>
+  );
+};
